3818580d9f633ba6f1e8caa53a4db250366f837a
henrynguyen <nguyenhenry682@gmail.com>
2025-03-15 20:54:06 -0500

    initial commit

---
+++ src/test/java/com/backend/h2ak/H2akApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.h2ak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2akApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
