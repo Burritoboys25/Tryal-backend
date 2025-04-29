eb14648f1efc69192bed8a206521bdb39d4bbdfd
Calvin Nguyen <cnguyenr42@gmail.com>
2025-04-29 15:50:00 -0700

    Create category crud endpoints

--- src/main/java/com/backend/h2ak/Category/CategoryController.java
+++ src/main/java/com/backend/h2ak/Category/CategoryController.java
@@ -1,7 +1,92 @@
 package com.backend.h2ak.Category;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.h2ak.Category.Service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // get all Categories
+    @GetMapping()
+    public ResponseEntity<List<Category>> getAllCategories() {
+        try {
+            List<Category> categories = new ArrayList<Category>(categoryService.getAllCategories());
+
+            if (categories.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(categories, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // get Category by ID
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable UUID categoryId) {
+        try {
+            Category category = categoryService.getCategoryById(categoryId);
+
+            if (category == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(category, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Create Category
+    @PostMapping()
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        try{
+            Category newCategory = categoryService.createCategory(category);
+            return new ResponseEntity<>(newCategory, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Patch Category
+    @PatchMapping("/{categoryId}")
+    public ResponseEntity<Category> updateCategoryById(@RequestBody Category category, @PathVariable UUID categoryId) {
+        try {
+            Category updatedCategory = categoryService.updateCategoryById(categoryId, category);
+
+            if (updatedCategory == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Delete Category
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<String> deleteCategoryById(@PathVariable UUID categoryId) {
+        try {
+            if (categoryService.deleteCategoryById(categoryId)) {
+                return new ResponseEntity<>("Category deleted successfully.", HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Category not found.", HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
