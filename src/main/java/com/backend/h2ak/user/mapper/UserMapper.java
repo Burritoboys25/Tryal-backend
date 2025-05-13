240f8ed3455581b497a7c8fc76e65d630bfcb39c
Calvin Nguyen <cnguyenr42@gmail.com>
2025-05-13 14:29:14 -0700

    Create userMapper class

---
+++ src/main/java/com/backend/h2ak/user/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.backend.h2ak.user.mapper;
+
+import com.backend.h2ak.user.User;
+import com.backend.h2ak.user.dto.UserDTO;
+
+public class UserMapper {
+    public static UserDTO mapUserDto(User user) {
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setUserId(user.getUserId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPhoneNumber(user.getPhoneNumber());
+        userDTO.setDateOfBirth(user.getDataOfBirth());
+        userDTO.setProfileImageUrl(user.getProfileImageUrl());
+        userDTO.setCreditBalance(user.getCreditBalance());
+        userDTO.setStripeCustomerId(user.getStripeCustomerId());
+
+        return userDTO;
+    }
+}
