a4adcc5ed9dcb90fe3de1225500f1702123a0215
Calvin Nguyen <cnguyenr42@gmail.com>
2025-05-13 14:19:33 -0700

    Create userDto class

--- src/main/java/com/backend/h2ak/user/dto/UserDTO.java
+++ src/main/java/com/backend/h2ak/user/dto/UserDTO.java
@@ -1,5 +1,88 @@
 package com.backend.h2ak.user.dto;
 
-public class UserDTO {
+import java.time.LocalDate;
+import java.util.UUID;
 
+public class UserDTO {
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate dateOfBirth;
+    private String profileImageUrl;
+    private Integer creditBalance;
+    private String stripeCustomerId;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Integer getCreditBalance() {
+        return creditBalance;
+    }
+
+    public void setCreditBalance(Integer creditBalance) {
+        this.creditBalance = creditBalance;
+    }
+
+    public String getStripeCustomerId() {
+        return stripeCustomerId;
+    }
+
+    public void setStripeCustomerId(String stripeCustomerId) {
+        this.stripeCustomerId = stripeCustomerId;
+    }
 }
