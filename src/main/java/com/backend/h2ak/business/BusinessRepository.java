a4ca6e7d0896d21775f9fb47f21cc58bd3b9456c
Calvin Nguyen <cnguyenr42@gmail.com>
2025-05-13 14:05:14 -0700

    Rename business folder to follow consistent naming convention

--- src/main/java/com/backend/h2ak/business/BusinessRepository.java
+++ src/main/java/com/backend/h2ak/business/BusinessRepository.java
@@ -1,4 +1,4 @@
-package com.backend.h2ak.Business;
+package com.backend.h2ak.business;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
