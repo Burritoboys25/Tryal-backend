d7bd6c885ad58e36226b95476af3f4a0ab385314
Calvin Nguyen <cnguyenr42@gmail.com>
2025-05-20 20:06:30 -0700

    Setup security dependencies

--- src/main/java/com/backend/h2ak/H2akApplication.java
+++ src/main/java/com/backend/h2ak/H2akApplication.java
@@ -2,8 +2,9 @@ package com.backend.h2ak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class H2akApplication {
 
 	public static void main(String[] args) {
