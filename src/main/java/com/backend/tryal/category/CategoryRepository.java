5bb53a06156c7e11bf655001233ddebf4ca58dd7
Henry <91310711+SX129@users.noreply.github.com>
2025-05-26 16:17:40 -0500

    Merge dev into prod (#4)
    * Create pull_request_template.md
    
    * Delete src/.github directory
    
    * Create pull_request_template.md
    
    * Feature/entity models (#2)
    
    * Rename user folder to follow consistent naming convention
    
    * Rename plan folder to follow consistent naming convention
    
    * Rename category folder to follow consistent naming convention
    
    * Rename business folder to follow consistent naming convention
    
    * Create userDto class
    
    * Create userMapper class
    
    * Create userResponse class
    
    * Refactor getallusers endpoint to use userdto
    
    * Add password hash to userDTO
    
    * Refactor user endpoints to use userDTO
    
    * Add unique constraint to email and fix gender code
    
    * Implement password encryption for users
    
    * Setup security dependencies
    
    * Fix dateOfBirth typo
    
    * Remove password hash from user dto
    
    * Update README.md
    
    * Feature/backend auth (#3)
    
    * renamed to tryal
    
    * added cors handling
    
    * updated values for deployment
    
    * added Dockerfile for deployment
    
    * added default properties
    
    ---------
    
    Co-authored-by: Burrito Boys <burritoboys25@gmail.com>
    Co-authored-by: Calvin Nguyen <77700030+calvinguyen@users.noreply.github.com>

---
+++ src/main/java/com/backend/tryal/category/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.backend.tryal.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
