5967ae772df156aa89b8520edd2dc76ec216a817
Calvin Nguyen <cnguyenr42@gmail.com>
2025-05-26 14:04:22 -0700

    Remove spring security annotation

--- src/main/java/com/backend/tryal/TryalApplication.java
+++ src/main/java/com/backend/tryal/TryalApplication.java
@@ -2,9 +2,8 @@ package com.backend.tryal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class TryalApplication {
 
 	public static void main(String[] args) {
