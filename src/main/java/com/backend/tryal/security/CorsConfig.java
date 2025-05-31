03783dfb39758e394a4848d19e5cd924c15476b6
Henry Nguyen <nguyenhenry682@gmail.com>
2025-05-30 21:23:11 -0500

    updated cors config

--- src/main/java/com/backend/tryal/security/CorsConfig.java
+++ src/main/java/com/backend/tryal/security/CorsConfig.java
@@ -6,13 +6,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            public void addCorsMapping(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "PUT", "PATCH", "POST", "DELETE").allowedOrigins("http://localhost:3000");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
