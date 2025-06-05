c7e3b37d14ed49ff5ee063a675e76ab1ed95f2ad
Calvin Nguyen <cnguyenr42@gmail.com>
2025-06-05 16:25:57 -0700

    Add BusinessMapper

---
+++ src/main/java/com/backend/tryal/business/mapper/BusinessMapper.java
@@ -0,0 +1,20 @@
+package com.backend.tryal.business.mapper;
+
+import com.backend.tryal.business.Business;
+import com.backend.tryal.business.dto.BusinessDTO;
+
+public class BusinessMapper {
+    public static BusinessDTO mapBusinessDTO(Business business) {
+        BusinessDTO businessDTO = new BusinessDTO();
+
+        businessDTO.setBusinessId(business.getBusinessId());
+        businessDTO.setName(business.getName());
+        businessDTO.setEmail(business.getEmail());
+        businessDTO.setWebsite(business.getWebsite());
+        businessDTO.setAddress(business.getAddress());
+        businessDTO.setPhoneNumber(business.getPhoneNumber());
+        businessDTO.setExperiences(business.getExperiences());
+
+        return businessDTO;
+    }
+}
