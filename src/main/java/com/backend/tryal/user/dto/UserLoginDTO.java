ca8f73ee30c2d15ee7baf56fe5c5def78fccefd5
Calvin Nguyen <cnguyenr42@gmail.com>
2025-07-16 20:06:30 -0700

    remove unnecessary getter setter for users

--- src/main/java/com/backend/tryal/user/dto/UserLoginDTO.java
+++ src/main/java/com/backend/tryal/user/dto/UserLoginDTO.java
@@ -3,7 +3,9 @@ package com.backend.tryal.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class UserLoginDTO {
 
     @Email(message = "Must be a valid email")
@@ -17,22 +19,6 @@ public class UserLoginDTO {
 
     public UserLoginDTO(String email, String password) {
         this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
