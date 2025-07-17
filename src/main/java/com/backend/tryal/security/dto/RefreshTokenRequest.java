c9fa9033e15cedd11d25a43a46ba541ae407ee18
Calvin Nguyen <cnguyenr42@gmail.com>
2025-07-16 20:08:47 -0700

    remove unnecessary getter setter for security dtos

--- src/main/java/com/backend/tryal/security/dto/RefreshTokenRequest.java
+++ src/main/java/com/backend/tryal/security/dto/RefreshTokenRequest.java
@@ -1,19 +1,10 @@
 package com.backend.tryal.security.dto;
 
+import lombok.Data;
+
+@Data
 public class RefreshTokenRequest {
     private String refreshToken;
 
     public RefreshTokenRequest() {}
-
-    public RefreshTokenRequest(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public void setRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
-    }
 }
