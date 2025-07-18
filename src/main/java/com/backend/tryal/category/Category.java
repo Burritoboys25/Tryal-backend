a562b9491151969fc72ab75bc532bca5ee70fbb5
Henry Nguyen <nguyenhenry682@gmail.com>
2025-07-18 12:30:31 -0500

    dropping database

--- src/main/java/com/backend/tryal/category/Category.java
+++ src/main/java/com/backend/tryal/category/Category.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,7 +14,7 @@ import java.util.UUID;
 @Table(name = "categories")
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
     @Column(name = "category_id")
     private UUID categoryId;
 
