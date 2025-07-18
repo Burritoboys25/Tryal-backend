fa7258158d37f1c791de9c6fb2346060fa1b4f77
Henry Nguyen <nguyenhenry682@gmail.com>
2025-07-18 12:26:38 -0500

    added custom queries

--- src/main/java/com/backend/tryal/tokenTransaction/TokenTransactionRepository.java
+++ src/main/java/com/backend/tryal/tokenTransaction/TokenTransactionRepository.java
@@ -1,6 +1,8 @@
 package com.backend.tryal.tokenTransaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,5 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface TokenTransactionRepository extends JpaRepository<TokenTransaction, UUID>{
-    List<TokenTransaction> findByUserId(UUID userId);
+    @Query("SELECT t FROM TokenTransaction t WHERE t.user.userId = :userId")
+    List<TokenTransaction> findByUserId(@Param("userId") UUID userId);
 }
