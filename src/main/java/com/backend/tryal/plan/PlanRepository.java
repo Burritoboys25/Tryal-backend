4e1ae3028ede00f523dab55dfb5ed0f4c526e70a
Henry Nguyen <nguyenhenry682@gmail.com>
2025-07-31 22:25:34 -0500

    added lookup for stripe id

--- src/main/java/com/backend/tryal/plan/PlanRepository.java
+++ src/main/java/com/backend/tryal/plan/PlanRepository.java
@@ -3,8 +3,10 @@ package com.backend.tryal.plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, UUID> {
+    Optional<Plan> findByStripePriceId(String stripePriceId);
 }
