9ef4ed68740376d450530477a6b7bb69dcfc1a20
Calvin Nguyen <cnguyenr42@gmail.com>
2025-08-29 12:07:16 -0700

    Refactor create token transaction controller and serviceimpl

--- src/main/java/com/backend/tryal/tokenTransaction/TokenTransactionController.java
+++ src/main/java/com/backend/tryal/tokenTransaction/TokenTransactionController.java
@@ -2,14 +2,11 @@ package com.backend.tryal.tokenTransaction;
 
 import com.backend.tryal.tokenTransaction.dto.TokenTransactionDTO;
 import com.backend.tryal.tokenTransaction.mapper.TokenTransactionMapper;
-import com.backend.tryal.tokenTransaction.response.TokenTransactionResponse;
 import com.backend.tryal.tokenTransaction.service.TokenTransactionService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,26 +55,11 @@ public class TokenTransactionController {
   }
 
   //create a transaction
-  @PostMapping("/users/{userId}")
-  public ResponseEntity<TokenTransactionResponse> createTokenTransaction(@PathVariable UUID userId,
+  @PostMapping("/user/{userId}")
+  public TokenTransactionDTO createTokenTransaction(@PathVariable UUID userId,
       @RequestBody TokenTransactionDTO tokenTransactionRequestDTO) {
-    try {
-      TokenTransactionDTO transactionDTO = TokenTransactionMapper.mapTokenTransactionDTO(userId,
-          tokenTransactionService.createTokenTransaction(userId, tokenTransactionRequestDTO));
-
-      if (transactionDTO == null) {
-        return new ResponseEntity<>(
-            new TokenTransactionResponse(null, "User with id: " + userId + " not found."),
-            HttpStatus.NOT_FOUND);
-      }
-
-      return new ResponseEntity<>(
-          new TokenTransactionResponse(transactionDTO, "Transaction created successfully."),
-          HttpStatus.CREATED);
-
-    } catch (Exception e) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    return TokenTransactionMapper.mapTokenTransactionDTO(userId,
+        tokenTransactionService.createTokenTransaction(userId, tokenTransactionRequestDTO));
   }
 
 }
--- src/main/java/com/backend/tryal/tokenTransaction/service/TokenTransactionServiceImpl.java
+++ src/main/java/com/backend/tryal/tokenTransaction/service/TokenTransactionServiceImpl.java
@@ -10,17 +10,17 @@ import com.backend.tryal.tokenTransaction.dto.TokenTransactionDTO;
 import com.backend.tryal.user.User;
 import com.backend.tryal.user.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.UUID;
+import org.springframework.stereotype.Service;
 
 @Service
-public class TokenTransactionServiceImpl implements TokenTransactionService{
-    private final TokenTransactionRepository tokenTransactionRepository;
-    private final UserRepository userRepository;
-    private final SubscriptionRepository subscriptionRepository;
-    private final BookingRepository bookingRepository;
+public class TokenTransactionServiceImpl implements TokenTransactionService {
+
+  private final TokenTransactionRepository tokenTransactionRepository;
+  private final UserRepository userRepository;
+  private final SubscriptionRepository subscriptionRepository;
+  private final BookingRepository bookingRepository;
 
   public TokenTransactionServiceImpl(TokenTransactionRepository tokenTransactionRepository,
       UserRepository userRepository, SubscriptionRepository subscriptionRepository,
@@ -32,47 +32,60 @@ public class TokenTransactionServiceImpl implements TokenTransactionService{
   }
 
   @Override
-    public List<TokenTransaction> getAllTokenTransactions() {
-        return tokenTransactionRepository.findAll();
+  public List<TokenTransaction> getAllTokenTransactions() {
+    return tokenTransactionRepository.findAll();
+  }
+
+  @Override
+  public List<TokenTransaction> getAllTokenTransactions(UUID userId) {
+    if (userRepository.findById(userId).orElse(null) == null) {
+      throw new EntityNotFoundException("Could not find user with id: " + userId);
     }
 
-    @Override
-    public List<TokenTransaction> getAllTokenTransactions(UUID userId) {
-      if (userRepository.findById(userId).orElse(null) == null) {
-        throw new EntityNotFoundException("Could not find user with id: " + userId);
+    return tokenTransactionRepository.findByUserId(userId);
+  }
+
+  @Override
+  public TokenTransaction getTokenTransactionById(UUID transactionId) {
+    if (tokenTransactionRepository.findById(transactionId).orElse(null) == null) {
+      throw new EntityNotFoundException("Could not find token with id: " + transactionId);
+    }
+
+    return tokenTransactionRepository.findById(transactionId).orElse(null);
+  }
+
+  @Override
+  public TokenTransaction createTokenTransaction(UUID userId,
+      TokenTransactionDTO tokenTransactionDTO) {
+    User user = userRepository.findById(userId).orElse(null);
+    if (user == null) {
+      throw new EntityNotFoundException("Could not find user with id: " + userId);
+    }
+
+    TokenTransaction transaction = new TokenTransaction();
+
+    Booking booking = null;
+    if (tokenTransactionDTO.getBookingId() != null) {
+      booking = bookingRepository.findById(tokenTransactionDTO.getBookingId()).orElse(null);
+      if (booking == null) {
+        throw new EntityNotFoundException("Could not find booking with id: " + userId);
       }
-
-        return tokenTransactionRepository.findByUserId(userId);
+      transaction.setBooking(booking);
     }
-
-    @Override
-    public TokenTransaction getTokenTransactionById(UUID transactionId) {
-      if (tokenTransactionRepository.findById(transactionId).orElse(null) == null) {
-        throw new EntityNotFoundException("Could not find token with id: " + transactionId);
+    Subscription subscription = null;
+    if (tokenTransactionDTO.getSubscriptionId() != null) {
+      subscription = subscriptionRepository.findById(
+          tokenTransactionDTO.getSubscriptionId()).orElse(null);
+      if (subscription == null) {
+        throw new EntityNotFoundException("Could not find subscription with id: " + userId);
       }
-
-        return tokenTransactionRepository.findById(transactionId).orElse(null);
+      transaction.setSubscription(subscription);
     }
 
-    @Override
-    public TokenTransaction createTokenTransaction(UUID userId, TokenTransactionDTO tokenTransactionDTO) {
-        User user = userRepository.findById(userId).orElse(null);
+    transaction.setUser(user);
+    transaction.setTransactionReason(tokenTransactionDTO.getTransactionReason());
+    transaction.setCreditAmount(tokenTransactionDTO.getCreditAmount());
 
-        Booking booking = bookingRepository.findById(tokenTransactionDTO.getBookingId()).orElse(null);
-        Subscription subscription = subscriptionRepository.findById(tokenTransactionDTO.getSubscriptionId()).orElse(null);
-
-        if(user == null){
-            return null;
-        }else if(subscription == null){
-            return null;
-        }
-
-        TokenTransaction transaction = new TokenTransaction();
-        transaction.setUser(user);
-        transaction.setTransactionReason(tokenTransactionDTO.getTransactionReason());
-        transaction.setCreditAmount(tokenTransactionDTO.getCreditAmount());
-        transaction.setSubscription(subscription);
-
-        return tokenTransactionRepository.save(transaction);
-    }
+    return tokenTransactionRepository.save(transaction);
+  }
 }
