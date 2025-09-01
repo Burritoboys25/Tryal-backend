91a9e3e0fe8a42cb47ec93ab9c9004e5a995eb39
Calvin Nguyen <cnguyenr42@gmail.com>
2025-08-31 17:15:30 -0700

    Refactor plans controller and serviceimpl

--- src/main/java/com/backend/tryal/plan/service/PlanServiceImpl.java
+++ src/main/java/com/backend/tryal/plan/service/PlanServiceImpl.java
@@ -4,65 +4,63 @@ import com.backend.tryal.plan.Plan;
 import com.backend.tryal.plan.PlanRepository;
 import com.backend.tryal.plan.dto.PlanDTO;
 import com.backend.tryal.plan.mapper.PlanMapper;
-import org.springframework.stereotype.Service;
-
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PlanServiceImpl implements PlanService {
-    private final PlanRepository planRepository;
 
-    public PlanServiceImpl(PlanRepository planRepository) {
-        this.planRepository = planRepository;
+  private final PlanRepository planRepository;
+
+  public PlanServiceImpl(PlanRepository planRepository) {
+    this.planRepository = planRepository;
+  }
+
+  @Override
+  public List<PlanDTO> getAllActivePlans() {
+    return planRepository.findAll().stream()
+        .filter(Plan::getIsActive)
+        .map(PlanMapper::mapPlanDTO)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public Plan getPlanById(UUID planId) {
+    Plan plan = planRepository.findById(planId).orElse(null);
+    if (plan == null) {
+      throw new EntityNotFoundException("Plan not found with id: " + planId);
+    }
+    return plan;
+  }
+
+  @Override
+  public Plan createPlan(Plan plan) {
+    planRepository.save(plan);
+    return plan;
+  }
+
+  @Override
+  public void deactivatePlanById(UUID planId) {
+    Plan plan = planRepository.findById(planId).orElse(null);
+    if (plan == null) {
+      throw new EntityNotFoundException("Plan not found with id: " + planId);
     }
 
-    @Override
-    public List<PlanDTO> getAllActivePlans() {
-        return planRepository.findAll().stream()
-                .filter(Plan::getIsActive)
-                .map(PlanMapper::mapPlanDTO)
-                .collect(Collectors.toList());
+    plan.setIsActive(false);
+    planRepository.save(plan);
+  }
+
+  @Override
+  public void reactivatePlanById(UUID planId) {
+    Plan plan = planRepository.findById(planId).orElse(null);
+    if (plan == null) {
+      throw new EntityNotFoundException("Plan not found with id: " + planId);
     }
 
-    @Override
-    public Plan getPlanById(UUID planId) {
-        return planRepository.findById(planId).orElse(null);
-    }
-
-    @Override
-    public Plan createPlan(Plan plan) {
-        planRepository.save(plan);
-
-        return plan;
-    }
-
-    @Override
-    public boolean deactivatePlanById(UUID planId) {
-        Plan plan = getPlanById(planId);
-
-        if(plan == null){
-            return false;
-        }
-
-        plan.setIsActive(false);
-        planRepository.save(plan);
-
-        return true;
-    }
-
-    @Override
-    public boolean reactivatePlanById(UUID planId) {
-        Plan plan = getPlanById(planId);
-
-        if(plan == null){
-            return false;
-        }
-
-        plan.setIsActive(true);
-        planRepository.save(plan);
-
-        return true;
-    }
+    plan.setIsActive(true);
+    planRepository.save(plan);
+  }
 }
